cabdd43bdb29c76dfe781ce8ab2629bb4bf1f1df
Michael Washam <michael@opsgility.com>
2017-06-03 07:27:55 -0500

    test

--- OMSEGDeploy.ps1
+++ OMSEGDeploy.ps1
@@ -4,7 +4,9 @@ Select-AzureRmSubscription -SubscriptionName "opsgilitytraining"
 $hqRG = "ContosoAzureHQ"
 $location = "East US"
 $hqTemplate = "https://raw.githubusercontent.com/opsgility/oms-evergreen/master/OMSEGDeploy/OMSEGDeploy/azuredeploy.json"
+$hqTemplateFile = "C:\repos\oms-evergreen\OMSEGDeploy\OMSEGDeploy\azuredeploy.json"
 
 New-AzureRmResourceGroup -Name $hqRG -Location $location -Force
 
-New-AzureRmResourceGroupDeployment -Name "ContosoAzureHQDeploy" -ResourceGroupName $hqRG -TemplateUri $hqTemplate -Mode Incremental 
+#New-AzureRmResourceGroupDeployment -Name "ContosoAzureHQDeploy" -ResourceGroupName $hqRG -TemplateUri $hqTemplate -Mode Incremental 
+New-AzureRmResourceGroupDeployment -Name "ContosoAzureHQDeploy" -ResourceGroupName $hqRG -TemplateFile $hqTemplateFile -Mode Incremental 
