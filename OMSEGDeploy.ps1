5bf9d53db95b1ae652a1288ddc40c5aa3e0f5d28
Michael Washam <michael@opsgility.com>
2017-06-03 08:22:20 -0500

    updates

--- OMSEGDeploy.ps1
+++ OMSEGDeploy.ps1
@@ -5,8 +5,15 @@ $hqRG = "ContosoAzureHQ"
 $location = "East US"
 $hqTemplate = "https://raw.githubusercontent.com/opsgility/oms-evergreen/master/OMSEGDeploy/OMSEGDeploy/azuredeploy.json"
 $hqTemplateFile = "C:\repos\oms-evergreen\OMSEGDeploy\OMSEGDeploy\azuredeploy.json"
-
+$webTemplateFile = "C:\repos\oms-evergreen\OMSEGDeploy\OMSEGDeploy\nested-templates\webapp.json"
 New-AzureRmResourceGroup -Name $hqRG -Location $location -Force
 
 #New-AzureRmResourceGroupDeployment -Name "ContosoAzureHQDeploy" -ResourceGroupName $hqRG -TemplateUri $hqTemplate -Mode Incremental 
 New-AzureRmResourceGroupDeployment -Name "ContosoAzureHQDeploy" -ResourceGroupName $hqRG -TemplateFile $hqTemplateFile -Mode Incremental 
+
+
+
+New-AzureRmResourceGroup -Name "webtest" -Location $location -Force
+
+#New-AzureRmResourceGroupDeployment -Name "ContosoAzureHQDeploy" -ResourceGroupName $hqRG -TemplateUri $hqTemplate -Mode Incremental 
+New-AzureRmResourceGroupDeployment -Name "ContosoAzureHQDeployWEb" -ResourceGroupName webtest -TemplateFile $webTemplateFile -Mode Incremental 
