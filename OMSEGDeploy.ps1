d07683520182d3440e3a4210774f8040dff01325
Michael Washam <michael@opsgility.com>
2017-06-05 07:14:57 -0500

    added VPN Shared Key param

--- OMSEGDeploy.ps1
+++ OMSEGDeploy.ps1
@@ -7,11 +7,14 @@ $AzureHQRG = "ContosoAzureHQ"
 $AzureHQTemplate = "https://raw.githubusercontent.com/opsgility/oms-evergreen/master/OMSEGDeploy/OMSEGDeploy/azuredeploy.json"
 
 $OnPremHQRG = "ContosoOnPremHQ"
-$OnPremHQRGTemplate = ""
+$OnPremHQRGTemplate = "https://raw.githubusercontent.com/opsgility/oms-evergreen/master/OMSEGDeploy/ContosoOnPremHQ/azuredeploy.json"
 
 New-AzureRmResourceGroup -Name $AzureHQRG -Location $location -Force
 New-AzureRmResourceGroupDeployment -Name "ContosoAzureHQDeploy" -ResourceGroupName $AzureHQRG -TemplateFile $AzureHQTemplate -Mode Incremental 
 
 
+# After deployement - retrieve public IP addresses and update gateways 
 
 
+#New-AzureRmResourceGroup -Name $OnPremHQRG -Location $location -Force
+#New-AzureRmResourceGroupDeployment -Name "ContosoOnPremHQDeploy" -ResourceGroupName $OnPremHQRG -TemplateFile $OnPremHQRGTemplate -Mode Incremental 
