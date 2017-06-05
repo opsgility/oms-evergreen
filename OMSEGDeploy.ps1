bb00ef9b8bb63074ebd72a058ce52a2511bf9336
Michael Washam <michael@opsgility.com>
2017-06-05 07:09:46 -0500

    added gateways to templates

--- OMSEGDeploy.ps1
+++ OMSEGDeploy.ps1
@@ -1,19 +1,17 @@
 cls
 Select-AzureRmSubscription -SubscriptionName "opsgilitytraining"
 
-$hqRG = "ContosoAzureHQ"
 $location = "East US"
-$hqTemplate = "https://raw.githubusercontent.com/opsgility/oms-evergreen/master/OMSEGDeploy/OMSEGDeploy/azuredeploy.json"
-$hqTemplateFile = "C:\repos\oms-evergreen\OMSEGDeploy\OMSEGDeploy\azuredeploy.json"
-$webTemplateFile = "C:\repos\oms-evergreen\OMSEGDeploy\OMSEGDeploy\nested-templates\webapp.json"
-New-AzureRmResourceGroup -Name $hqRG -Location $location -Force
 
-#New-AzureRmResourceGroupDeployment -Name "ContosoAzureHQDeploy" -ResourceGroupName $hqRG -TemplateUri $hqTemplate -Mode Incremental 
-New-AzureRmResourceGroupDeployment -Name "ContosoAzureHQDeploy" -ResourceGroupName $hqRG -TemplateFile $hqTemplateFile -Mode Incremental 
+$AzureHQRG = "ContosoAzureHQ"
+$AzureHQTemplate = "https://raw.githubusercontent.com/opsgility/oms-evergreen/master/OMSEGDeploy/OMSEGDeploy/azuredeploy.json"
+
+$OnPremHQRG = "ContosoOnPremHQ"
+$OnPremHQRGTemplate = ""
+
+New-AzureRmResourceGroup -Name $AzureHQRG -Location $location -Force
+New-AzureRmResourceGroupDeployment -Name "ContosoAzureHQDeploy" -ResourceGroupName $AzureHQRG -TemplateFile $AzureHQTemplate -Mode Incremental 
 
 
 
-New-AzureRmResourceGroup -Name "webtest" -Location $location -Force
 
-#New-AzureRmResourceGroupDeployment -Name "ContosoAzureHQDeploy" -ResourceGroupName $hqRG -TemplateUri $hqTemplate -Mode Incremental 
-New-AzureRmResourceGroupDeployment -Name "ContosoAzureHQDeployWEb" -ResourceGroupName webtest -TemplateFile $webTemplateFile -Mode Incremental 
