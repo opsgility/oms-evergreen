50b20cff177032f7f32863250dad2b497e542e59
Michael Washam <michael@opsgility.com>
2017-06-06 20:42:07 -0500

    hacking scripts

--- script-extensions/deploy-cloudshop-db.ps1
+++ script-extensions/deploy-cloudshop-db.ps1
@@ -1,40 +1,31 @@
-param($domain, $user, $password, $dbsource)
+param($domain, $user, $password, $dbsource, $sqlConfigUrl)
 
 $logs = "C:\Logs"
 $data = "C:\Data"
 $backups = "C:\Backup" 
+$script  = "C:\Script" 
+
 [system.io.directory]::CreateDirectory($logs)
 [system.io.directory]::CreateDirectory($data)
 [system.io.directory]::CreateDirectory($backups)
+[system.io.directory]::CreateDirectory($script)
 [system.io.directory]::CreateDirectory("C:\SQLDATA")
 
-# Setup the data, backup and log directories as well as mixed mode authentication
-Import-Module "sqlps" -DisableNameChecking
-[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
-$sqlesq = new-object ('Microsoft.SqlServer.Management.Smo.Server') Localhost
-$sqlesq.Settings.LoginMode = [Microsoft.SqlServer.Management.Smo.ServerLoginMode]::Mixed
-$sqlesq.Settings.DefaultFile = $data
-$sqlesq.Settings.DefaultLog = $logs
-$sqlesq.Settings.BackupDirectory = $backups
-$sqlesq.Alter() 
+$splitpath = $sqlConfigUrl.Split("/")
+$fileName = $splitpath[$splitpath.Length-1]
+$destinationPath = "$script\configure-sql.ps1"
 
-# Restart the SQL Server service
-Restart-Service -Name "MSSQLSERVER" -Force
-# Re-enable the sa account and set a new password to enable login
-Invoke-Sqlcmd -ServerInstance Localhost -Database "master" -Query "ALTER LOGIN sa ENABLE"
-Invoke-Sqlcmd -ServerInstance Localhost -Database "master" -Query "ALTER LOGIN sa WITH PASSWORD = 'Demo@pass1'"
+(New-Object Net.WebClient).DownloadFile($sqlConfigUrl,$destinationPath);
 
-# Get the Adventure works database backup 
-$dbdestination = "C:\SQLDATA\AdventureWorks2012.bak"
-Invoke-WebRequest $dbsource -OutFile $dbdestination 
+$password =  ConvertTo-SecureString "$password" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$user", $password)
+$command = $file = $PSScriptRoot + "\configure-sql.ps1"
+Enable-PSRemoting –force
+Invoke-Command -FilePath $destinationPath -Credential $credential -ComputerName $env:COMPUTERNAME
+Disable-PSRemoting -Force
 
-$mdf = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("AdventureWorks2012_Data", "C:\Data\AdventureWorks2012.mdf")
-$ldf = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("AdventureWorks2012_Log", "C:\Logs\AdventureWorks2012.ldf")
-
-# Restore the database from the backup
-Restore-SqlDatabase -ServerInstance Localhost -Database AdventureWorks `
-			-BackupFile $dbdestination -RelocateFile @($mdf,$ldf)  
 New-NetFirewallRule -DisplayName "SQL Server" -Direction Inbound –Protocol TCP –LocalPort 1433 -Action allow 
+
 
 
 $smPassword = (ConvertTo-SecureString $password -AsPlainText -Force)
