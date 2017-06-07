d17f8e5a8c053047feb974a5f9e22e244ceb1724
Michael Washam <michael@opsgility.com>
2017-06-06 19:21:44 -0500

    updates

--- script-extensions/deploy-cloudshop.ps1
+++ script-extensions/deploy-cloudshop.ps1
@@ -1,19 +1,37 @@
 param($domain, $user, $password, $cloudShopUrl)
 
+
+
 add-WindowsFeature -Name "Web-Server" -IncludeAllSubFeature
 
 
-$splitpath = $cloudShopUrl.Split("/")
-$fileName = $splitpath[$splitpath.Length-1]
-$destinationPath = "C:\Inetpub\wwwroot"
 
-(New-Object Net.WebClient).DownloadFile($cloudShopUrl,$destinationPath);
+
+$splitpath = $cloudShopUrl.Split("/")
+
+$fileName = $splitpath[$splitpath.Length-1]
+
+$destinationPath = "C:\Inetpub\wwwroot\CloudShop.zip"
+
+$destinationFolder = "C:\Inetpub\wwwroot"
+
+
+
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile($cloudShopUrl,$destinationPath)
+
+
 
 (new-object -com shell.application).namespace($destinationFolder).CopyHere((new-object -com shell.application).namespace($destinationPath).Items(),16)
 
 
 
+
+
 $smPassword = (ConvertTo-SecureString $password -AsPlainText -Force)
+
 $user = "$domain\$user"
+
 $objCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($user, $smPassword)
+
 Add-Computer -DomainName "$domain" -Credential $objCred -Restart -Force
